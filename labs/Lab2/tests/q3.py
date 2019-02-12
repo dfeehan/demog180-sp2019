467ebed8cf4de63680ba7bb676c588dc38ef40ff
Mia-Zhong <miazhongrj@gmail.com>
2019-02-12 11:25:17 -0800

    added materials for lab2

---
+++ labs/Lab2/tests/q3.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'type':'wwpp',
+      'cases': [
+        {
+          
+          'code': """
+          >>> round(g_degrees.num_rows,1) == 69.0
+          True
+          """,
+          'hidden': False,
+          'locked': True
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ labs/Lab2/tests2/q4.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'type':'wwpp',
+      'cases': [
+        {
+          
+          'code': """
+          >>> round(q4,2) == 1.25
+          True
+          """,
+          'hidden': False,
+          'locked': True
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
